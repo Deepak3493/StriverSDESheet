56d391d7e7f585af9035f00ebdd67a56a210f4ba
Deepak3493 <59064357+Deepak3493@users.noreply.github.com>
2022-04-26 02:59:03 +0530

    Create job_sequencing_problem.java

---
+++ greedyAlgorithms/job_sequencing_problem.java
@@ -0,0 +1,132 @@
+/*
+Job Sequencing Problem 
+Medium Accuracy: 48.94% Submissions: 65765 Points: 4
+Given a set of N jobs where each jobi has a deadline and profit associated with it.
+
+Each job takes 1 unit of time to complete and only one job can be scheduled at a time. We earn the profit associated with job if and only if the job is completed by its deadline.
+
+Find the number of jobs done and the maximum profit.
+
+Note: Jobs will be given in the form (Jobid, Deadline, Profit) associated with that Job.
+
+
+Example 1:
+
+Input:
+N = 4
+Jobs = {(1,4,20),(2,1,10),(3,1,40),(4,1,30)}
+Output:
+2 60
+Explanation:
+Job1 and Job3 can be done with
+maximum profit of 60 (20+40).
+Example 2:
+
+Input:
+N = 5
+Jobs = {(1,2,100),(2,1,19),(3,2,27),
+        (4,1,25),(5,1,15)}
+Output:
+2 127
+Explanation:
+2 jobs can be done with
+maximum profit of 127 (100+27).
+
+Your Task :
+You don't need to read input or print anything. Your task is to complete the function JobScheduling() which takes an integer N and an array of Jobs(Job id, Deadline, Profit) as input and returns the count of jobs and maximum profit.
+
+
+Expected Time Complexity: O(NlogN)
+Expected Auxilliary Space: O(N)
+
+
+Constraints:
+1 <= N <= 105
+1 <= Deadline <= 100
+1 <= Profit <= 500
+*/
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        Arrays.sort(arr,new comparator());
+        int ans[]=new int[n];
+        int finalProfit=0;
+        int JobsDone=0;
+        for(int i=0;i<n;i++){
+            Job j=arr[i];
+            int id=j.id;
+            int profit=j.profit;
+            int deadline=j.deadline;
+            deadline-=1;
+            while(deadline>=0){
+                if(deadline>=n){
+                    deadline--;
+                }
+                else if(ans[deadline]!=0){
+                    deadline--;
+                }
+                else{
+                    ans[deadline]=id;
+                    finalProfit+=profit;
+                    JobsDone++;
+                    break;
+                }
+            }
+        }
+        int ansarr[]=new int[2];
+        ansarr[0]=JobsDone;
+        ansarr[1]=finalProfit;
+        return ansarr;
+    }
+}
+class comparator implements Comparator<Job>{
+    public int compare(Job j1 , Job j2){
+        return j2.profit-j1.profit;
+    }
+}
